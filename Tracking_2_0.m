eea0688f3e63b2b676fea2d252c9914089381283
NotAnHerb <sage.r.aronson@gmail.com>
2016-09-14 16:48:58 -0700

    Is animal in ROI?
    Updated program to detect if an animal is in a user defined ROI. It can
    do this based on the centroid, the ellipsis described around the
    animal, or the animal itself.
    
    The main issue is tracking the animal’s head specifically. Once we can
    do this, the rest of the process will be trivial.

--- Tracking_2_0.m
+++ Tracking_2_0.m
@@ -5,7 +5,7 @@ tic
 for i = 1:n_frames
     image = Test.image{i};
     image(image<(prctile(reshape(image,[],1),98))) = 0; %thresholds the image (quick process) to only look at top 2% of pixels -- will only work with infrared camera
-    image(image>=(prctile(reshape(image,[],1),98))) = 1;
+%     image(image>=(prctile(reshape(image,[],1),98))) = 1;
     border {i} = image; %saves thresholded image
 
 end
@@ -18,35 +18,56 @@ for i = 1:n_frames
     
     BWdfill = imfill(border{i}, 'holes'); %fills in thresholded images
     BWdfill = bwareaopen(BWdfill,600,8); %excludes any region less than 600 pixels
+    
     border2{i} = BWdfill; %saves as border2
  
 end
 
-%%
 
+%%
+%make an arbitrary ROI
+close all
+
+figure
+
+imagesc(border2{1})
+
+[BW, xi, yi] = roipoly;
+hold on
+
+plot(xi,yi,'m')
+
+[BW2, xi_2, yi_2] = roipoly;
+plot(xi_2,yi_2,'y')
+
+pause
+close all
+%%
+%This code needs to track the animal's head and do some function when it
+%detects the head inside of a user defined ROI(s)
 %visualization of tracking data
 close all
 figure
-for i = 2:n_frames
+for i = 1:n_frames
     
     %plots original image
-    subplot(4,1,1)
-    imagesc(Test.image{i})
-    colormap('gray')
+%     subplot(4,1,1) 
+
+%     imagesc(Test.image{i})
+%     colormap('gray')
     
     %plots image as it is processed so far
-    subplot(4,1,2)
     imagesc(border2{i});
     hold on
     
+    
     s = regionprops(border2{i},'centroid','majoraxislength','minoraxislength',...
         'boundingbox','Orientation',...
-        'Perimeter','FilledArea'); %saves a bunch of parameters in structure s
+        'Perimeter','FilledArea','convexhull','conveximage'); %saves a bunch of parameters in structure s
     
-    %plots centroid on animal
-    scatter(s(1).Centroid(1),s(1).Centroid(2),'k');
     
-    %plots ellipsis around animal (code based on Steve Eddin's code:
+    
+     %plots ellipsis around animal (code based on Steve Eddin's code:
     %http://blogs.mathworks.com/steve/2010/07/30/visualizing-regionprops-ellipse-measurements/)
     
     phi = linspace(0,2*pi,50);
@@ -72,9 +93,63 @@ for i = 2:n_frames
 
     plot(x,y,'r','LineWidth',2);
     
+    
+    
+    
+    
+    
+    
+    
+    
+    %plots ROI on image -- is maroon if animal is outside ROI, yellow if
+    %animal is inside
+ 
+    %makes mask from ROI polygon
+    
+    test_mat = poly2mask(xi,yi,480,640); %for ROI1
+    test_mat_2 = poly2mask(xi_2,yi_2,480,640); %for ROI2
+    test_elps = poly2mask(x,y,480,640); %for ellipsis
+    
+    
+    
+    
+    
+    
+%     in_small = inpolygon(s(1).ConvexHull(:,1),s(1).ConvexHull(:,2),xi,yi); %inside small roi?
+%     in_big = inpolygon(s(1).ConvexHull(:,1),s(1).ConvexHull(:,2),xi_2,yi_2); %inside large roi?
+
+        
+        %if there are non-zero elements of the overlapping matrix of the
+        %thresholded image and the ROI then plot ROI in a given color
+        
+        %can also track base on the ellipsis drawn around the animal...
+
+    if nnz(test_mat_2 & test_elps) > 0; %is animal in big ROI?
+        plot(xi_2,yi_2,'y')
+    else
+        plot(xi_2,yi_2,'m')
+    end
+    
+    if nnz(test_mat & test_elps) > 0;
+        fill(xi,yi,'g')
+    else
+        plot(xi,yi,'m')
+    end
+    
+    
+    
+    
+    
+    
+    
+    %plots centroid on animal
+    scatter(s(1).Centroid(1),s(1).Centroid(2),'k');
+    
+   
+    
  % plots rectange around animal
     
-    rectangle('position',s(1).BoundingBox)
+    %rectangle('position',s(1).BoundingBox)
     
    
     
@@ -82,17 +157,16 @@ for i = 2:n_frames
  %elongation" while minor axis may help us identify the tail of the animal
  %-- especially if we can take cross sections through the ellipsis
     
-    subplot(4,1,3)
-    scatter(i,s(1).MajorAxisLength,'b')
-    scatter(i,s(1).MinorAxisLength,'m')
-    hold on
+%     subplot(4,1,3)
+%     scatter(i,s(1).MajorAxisLength,'b')
+%     scatter(i,s(1).MinorAxisLength,'m')
+%     hold on
     
     
     
     %plots perimeter
-    subplot(4,1,4)
-    scatter(i,s(1).Perimeter)
-    hold on
+ 
+    %
     
     %in progress -plotting orientation of animal
     
