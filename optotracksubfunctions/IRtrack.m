0b14f05fce4f6acd6c7afae20f74144733c0e93e
Bradley Monk <brad.monk@gmail.com>
2016-09-22 12:27:29 -0700

    gave function it's own "end"

--- optotracksubfunctions/IRtrack.m
+++ optotracksubfunctions/IRtrack.m
@@ -73,131 +73,140 @@ threshold = 98;
 
 
 
-for i = 1:2:n_frames
+    for i = 1:2:n_frames
 
-    %thresholds image
-    image = outputstructure.image{i};
-    image(image<(prctile(reshape(image,[],1),threshold))) = 0; 
-    image(image>=(prctile(reshape(image,[],1),threshold))) = 1;
-   
-    %find regions with contiguous pixels and makes 'objects'
-    CC = bwconncomp(image);
-    temp = regionprops(CC, 'Area','centroid',...
-        'majoraxislength','minoraxislength',...
-        'Orientation');
-    %and picks only the largest one
-    L = labelmatrix(CC);
-    [M I] = max([temp.Area]);
-    BW2 = ismember(L,I);
-    
+        %thresholds image
+        image = outputstructure.image{i};
+        image(image<(prctile(reshape(image,[],1),threshold))) = 0; 
+        image(image>=(prctile(reshape(image,[],1),threshold))) = 1;
 
-    BW{i} = BW2; %saves thresholded image
-    BW_region(i) = temp(I); %saves regionprops for large object in image
-
-  
-%uses regionprops to plot majoraxis, at the centroid, at the set
-%orientation -- note this can also draw an oval around the animal -- the
-%main output is x(1) y(1) and x(2) y(2) which are the points at either end
-%of the animal (roughly)
+        %find regions with contiguous pixels and makes 'objects'
+        CC = bwconncomp(image);
+        temp = regionprops(CC, 'Area','centroid',...
+            'majoraxislength','minoraxislength',...
+            'Orientation');
+        %and picks only the largest one
+        L = labelmatrix(CC);
+        [M I] = max([temp.Area]);
+        BW2 = ismember(L,I);
 
 
-%note it would be very easy to track the animal based on its centerpoint
-%instead of its head -- this function could be helpful for certain
-%experiments
+        BW{i} = BW2; %saves thresholded image
+        BW_region(i) = temp(I); %saves regionprops for large object in image
 
-    phi = linspace(0,2*pi,3);
-    cosphi = cos(phi);
-    sinphi = sin(phi);
 
-    
-    xbar = BW_region(i).Centroid(1);
-    ybar = BW_region(i).Centroid(2);
+    %uses regionprops to plot majoraxis, at the centroid, at the set
+    %orientation -- note this can also draw an oval around the animal -- the
+    %main output is x(1) y(1) and x(2) y(2) which are the points at either end
+    %of the animal (roughly)
 
-    a = BW_region(i).MajorAxisLength/2;
-    b = BW_region(i).MinorAxisLength/2;
 
-    theta = pi*BW_region(i).Orientation/180;
-    R = [ cos(theta)   sin(theta)
-         -sin(theta)   cos(theta)];
+    %note it would be very easy to track the animal based on its centerpoint
+    %instead of its head -- this function could be helpful for certain
+    %experiments
 
-    xy = [a*cosphi; b*sinphi];
-    xy = R*xy;
+        phi = linspace(0,2*pi,3);
+        cosphi = cos(phi);
+        sinphi = sin(phi);
 
-    x = xy(1,:) + xbar;
-    y = xy(2,:) + ybar;
-    
-    x = x(1:2);
-    y = y(1:2);
-    
- 
-%makes circular mask with radius r around x(1) y(1) -- outputs values
-%'testing' which is the mean pixel value in that region
 
-    cx=x(1);cy=y(1);
-    [x_3,y_3]=meshgrid(-(cx-1):(ix-cx),-(cy-1):(iy-cy));
-    c_mask_1=((x_3.^2+y_3.^2)<=r^2);
-    
+        xbar = BW_region(i).Centroid(1);
+        ybar = BW_region(i).Centroid(2);
 
-    testing = mean(mean(c_mask_1 .* BW{i}));
-  
-    
-%makes circular mask with radius r around x(2) y(2) -- outputs values
-%'testing2' which is the mean pixel value in that region
+        a = BW_region(i).MajorAxisLength/2;
+        b = BW_region(i).MinorAxisLength/2;
 
-    
-    cx=x(2);cy=y(2);
-    [x_3,y_3]=meshgrid(-(cx-1):(ix-cx),-(cy-1):(iy-cy));
-    c_mask_2=((x_3.^2+y_3.^2)<=r^2);
-    
- 
-    testing2 = mean(mean(c_mask_2 .* BW{i}));
-    
+        theta = pi*BW_region(i).Orientation/180;
+        R = [ cos(theta)   sin(theta)
+             -sin(theta)   cos(theta)];
 
-%whichever mask has a larger mean value is the head -- so make that value
-%'head_mask'
+        xy = [a*cosphi; b*sinphi];
+        xy = R*xy;
 
-    if testing > testing2
-        head_mask{i} = (c_mask_1.* BW{i});
-    else
-     
-        head_mask{i} = (c_mask_2.* BW{i});
+        x = xy(1,:) + xbar;
+        y = xy(2,:) + ybar;
+
+        x = x(1:2);
+        y = y(1:2);
+
+
+    %makes circular mask with radius r around x(1) y(1) -- outputs values
+    %'testing' which is the mean pixel value in that region
+
+        cx=x(1);cy=y(1);
+        [x_3,y_3]=meshgrid(-(cx-1):(ix-cx),-(cy-1):(iy-cy));
+        c_mask_1=((x_3.^2+y_3.^2)<=r^2);
+
+
+        testing = mean(mean(c_mask_1 .* BW{i}));
+
+
+    %makes circular mask with radius r around x(2) y(2) -- outputs values
+    %'testing2' which is the mean pixel value in that region
+
+
+        cx=x(2);cy=y(2);
+        [x_3,y_3]=meshgrid(-(cx-1):(ix-cx),-(cy-1):(iy-cy));
+        c_mask_2=((x_3.^2+y_3.^2)<=r^2);
+
+
+        testing2 = mean(mean(c_mask_2 .* BW{i}));
+
+
+    %whichever mask has a larger mean value is the head -- so make that value
+    %'head_mask'
+
+        if testing > testing2
+            head_mask{i} = (c_mask_1.* BW{i});
+        else
+
+            head_mask{i} = (c_mask_2.* BW{i});
+        end
+
+
+        head_center{i} = regionprops(head_mask{i},'centroid');
+
+
+        if mean(mean(STIM_region .* head_mask{i})) > 0 
+
+            %some rule -- ie -- a pulse that goes through the DAC
+
+        else
+
+
+        end
+
+
+
+
+
+       %plots a video of the thresholded image, the user_defined ROI, the
+       %centroid of the head_mask, and changes the color of the ROI is the
+       %animal is detected inside it
+
+       %would be cool if in the live-tracking mode -- if we could visualize the
+       %ROIs overlaid on the live video as well as whether the animal is being
+       %detected in a given ROI ... sort of like these videos
+
+       %plot video and tracking
+    % %    imagesc(BW{i})
+    % %    hold on
+    % %    plot(STIM_region_x, STIM_region_y,'k')
+    % %    scatter(head_center{i}.Centroid(1),head_center{i}.Centroid(2),'k')
+    % %    
+    % %    if mean(mean(STIM_region.*head_mask{i})) > 0
+    % %        plot(STIM_region_x, STIM_region_y,'y')
+    % %    else
+    % %    end
+    % %    
+    % %    pause(.05)
+
     end
-    
-       head_center{i} = regionprops(head_mask{i},'centroid');
-   
-   
-     if mean(mean(STIM_region.*head_mask{i})) > 0 
-       %some rule -- ie -- a pulse that goes through the DAC
-   else
-   end
-    
 
-    
- 
-   
-   %plots a video of the thresholded image, the user_defined ROI, the
-   %centroid of the head_mask, and changes the color of the ROI is the
-   %animal is detected inside it
-   
-   %would be cool if in the live-tracking mode -- if we could visualize the
-   %ROIs overlaid on the live video as well as whether the animal is being
-   %detected in a given ROI ... sort of like these videos
-   
-   %plot video and tracking
-% %    imagesc(BW{i})
-% %    hold on
-% %    plot(STIM_region_x, STIM_region_y,'k')
-% %    scatter(head_center{i}.Centroid(1),head_center{i}.Centroid(2),'k')
-% %    
-% %    if mean(mean(STIM_region.*head_mask{i})) > 0
-% %        plot(STIM_region_x, STIM_region_y,'y')
-% %    else
-% %    end
-% %    
-% %    pause(.05)
-    
+
+
+
+
 end
-
-
 %won't be necessary in function
 %clearvars -except BW Test head_center head_mask STIM_region STIM_region_x STIM_region_y
