2a8fdcaedfcc4cd8267325cabbbb6b3dee459ae1
Bradley Monk <brad.monk@gmail.com>
2016-09-22 17:44:52 -0700

    any pixels

--- optotracksubfunctions/livetracking.m
+++ optotracksubfunctions/livetracking.m
@@ -92,6 +92,12 @@ pause(.5)
 memos = memologs(memos, memoboxH, 'Mask is set.');
 
 
+axes(haxMAIN)
+ph1 = imagesc(STIM_region);
+set(gca,'YDir','reverse')
+haxMAIN.XLim = [1 size(IMG,2)];
+haxMAIN.YLim = [1 size(IMG,1)];
+hold on
 
 
 
@@ -143,7 +149,6 @@ triggerconfig(vidObj, 'manual');
 start(vidObj);
 
 
-
 %% START IMAGE ACQUISITION LOOP
 
 xy=[0 0];
@@ -177,11 +182,15 @@ for trial = 1:total_trials
             
             headmask = findsubject(IMG, threshmask, headrad, imsz);
         
-        
-            % FIND ALL NON-ZERO PIXELS (PIXELS ABOVE THRESHOLD)
-            [row,col,val] = find(headmask); 
-            RCV = [row,col,val];
-        
+            % anypixels = any(any(headmask .* STIM_region));
+            anypixels = sum(sum(headmask .* STIM_region));
+            
+            ph1.CData = IMG;
+            
+            memos = memologs(memos, memoboxH, ['Any pixels in ROI?: ' num2str(anypixels)]);
+            
+            drawnow
+                    
         else
             
             % FIND ALL NON-ZERO PIXELS (PIXELS ABOVE THRESHOLD)
@@ -189,22 +198,24 @@ for trial = 1:total_trials
             RCV = [row,col,val];
             
         
+            % SORT BY PIXEL BRIGHTNESS (HIGH TO LOW)
+            [YXv, index] = sortrows(RCV,-3); 
+
+            % GET THE COLUMN (X) AND ROW (Y) INDEX OF THE HOTTEST PIXELS
+            % MEAN OF THOSE PIXEL COORDINATES IS GEOMETRIC CENTER
+            xcol = mean( YXv(1:npixels,2) );
+
+            yrow = mean( imSizeY - YXv(1:npixels,1) );
+            % yrow = mean( YXv(1:npixels,1) );
+
+            xy(ff,:) = [xcol yrow];
+            
         end
         
         
         
         
-        % SORT BY PIXEL BRIGHTNESS (HIGH TO LOW)
-        [YXv, index] = sortrows(RCV,-3); 
-        
-        % GET THE COLUMN (X) AND ROW (Y) INDEX OF THE HOTTEST PIXELS
-        % MEAN OF THOSE PIXEL COORDINATES IS GEOMETRIC CENTER
-        xcol = mean( YXv(1:npixels,2) );
-        
-        yrow = mean( imSizeY - YXv(1:npixels,1) );
-        % yrow = mean( YXv(1:npixels,1) );
-        
-        xy(ff,:) = [xcol yrow];
+
         
         ff=ff+1;
     end
@@ -235,63 +246,71 @@ end
 % save([sub_dir, '/' outfile],'trial_data', 'Frames', 'FramesTS', 'FrameOrd');
 
 
+
+
+
+
+
+
 %% OPTIONAL: Play VIDEO overlaying tracking position over rat to check accuracy
 
-figure(mainguih);
+if ~trackhead
 
-% hax1 = axes('Position',[.05 .05 .9 .9],'Color','none','NextPlot','replacechildren');
-% hax2 = axes('Position',[.05 .05 .9 .9],'Color','none','XTick',[],'YTick',[],'NextPlot','replacechildren');
-% hax3 = axes('Position',[.05 .05 .9 .9],'Color','none','XTick',[],'YTick',[],'NextPlot','replacechildren');
-% axis off; hold on;
+    figure(mainguih);
 
-axes(haxMAIN)
-ph1 = imagesc(Frames(:,:,1));
-set(gca,'YDir','reverse')
-haxMAIN.XLim = [1 size(IMG,2)];
-haxMAIN.YLim = [1 size(IMG,1)];
-hold on
+    % hax1 = axes('Position',[.05 .05 .9 .9],'Color','none','NextPlot','replacechildren');
+    % hax2 = axes('Position',[.05 .05 .9 .9],'Color','none','XTick',[],'YTick',[],'NextPlot','replacechildren');
+    % hax3 = axes('Position',[.05 .05 .9 .9],'Color','none','XTick',[],'YTick',[],'NextPlot','replacechildren');
+    % axis off; hold on;
 
-axLims = axis;
+    axes(haxMAIN)
+    ph1 = imagesc(Frames(:,:,1));
+    set(gca,'YDir','reverse')
+    haxMAIN.XLim = [1 size(IMG,2)];
+    haxMAIN.YLim = [1 size(IMG,1)];
+    hold on
 
-% axes(hax2)
-ph2 = scatter(xy(1,1),xy(1,2),100,'filled','MarkerFaceColor',[.9 .1 .1]);
-axis(axLims)
-hold on
+    axLims = axis;
+
+    % axes(hax2)
+    ph2 = scatter(xy(1,1),xy(1,2),100,'filled','MarkerFaceColor',[.9 .1 .1]);
+    axis(axLims)
+    hold on
 
 
-% axes(hax3)
-ph3 = plot(xy(1,1),xy(1,2),'r');
-axis(axLims)
-hold on
-    
+    % axes(hax3)
+    ph3 = plot(xy(1,1),xy(1,2),'r');
+    axis(axLims)
+    hold on
 
-t = (1:size(xy,1));
-ts = linspace(1, size(xy,1), size(xy,1)*100 );
-xys = spline(t,xy',ts)';
 
-mm=1;
-for nn=1:size(Frames,3)
+    t = (1:size(xy,1));
+    ts = linspace(1, size(xy,1), size(xy,1)*100 );
+    xys = spline(t,xy',ts)';
 
-    ph1.CData = Frames(:,:,nn);
-    ph2.XData = xy(nn,1);
-    ph2.YData = xy(nn,2);
-    % ph3.XData = xy(1:nn,1);
-    % ph3.YData = xy(1:nn,2);
-    ph3.XData = xys(1:mm,1);
-    ph3.YData = xys(1:mm,2);
-    
-    pause(.2)
+    mm=1;
+    for nn=1:size(Frames,3)
 
-    mm = nn*100-1;
+        ph1.CData = Frames(:,:,nn);
+        ph2.XData = xy(nn,1);
+        ph2.YData = xy(nn,2);
+        % ph3.XData = xy(1:nn,1);
+        % ph3.YData = xy(1:nn,2);
+        ph3.XData = xys(1:mm,1);
+        ph3.YData = xys(1:mm,2);
+
+        pause(.2)
+
+        mm = nn*100-1;
+    end
+
+    % axes(hax3)
+    ph3 = plot(xys(:,1)+2,xys(:,2)+.5,'Color',[.99 .6 .90],'LineWidth',5);
+    ph3 = plot(xys(:,1),xys(:,2),'Color',[.99 .1 .75],'LineWidth',2);
+
+    axis(axLims)
+
 end
-
-% axes(hax3)
-ph3 = plot(xys(:,1)+2,xys(:,2)+.5,'Color',[.99 .6 .90],'LineWidth',5);
-ph3 = plot(xys(:,1),xys(:,2),'Color',[.99 .1 .75],'LineWidth',2);
-
-axis(axLims)
-
-
 
 
 % fh1=figure('Units','normalized','OuterPosition',[.1 .1 .6 .7],'Color','w','MenuBar','none');
